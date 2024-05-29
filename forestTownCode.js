602921e37233d938165109b4cc1c91afc1db0e8d
worldreset <112832918+worldreset@users.noreply.github.com>
2024-05-28 23:38:49 -0400

    Add files via upload

--- forestTownCode.js
+++ forestTownCode.js
@@ -14,34 +14,13 @@ function displayContent(content) {
   photo.setAttribute("alt", content.image.alt);
 }
 
+// When page is loaded, get p which is the query string. 
+const urlParams = new URLSearchParams(window.location.search);
+const imageParameter = urlParams.get('p');
+console.log(imageParameter);
+
 
 document.addEventListener("click", async (event) => {
-  if (imageParameter) {
-    event.preventDefault();
-    try {
-      // When page is loaded, fetch the JSON associated with the query string, "p".
-      const response = await fetch(`forestImagesJSON/${imageParameter}.json`);
-
-      console.log(response);
-
-      const json = await response.json(); // continued from above
-      displayContent(json);
-      console.log(json);  
-    } catch (err) {
-      console.error(err);
-    }
-  }
-});
-
-document.addEventListener("load", async (event) => {
-
-  
-  // When page is loaded, get p which is the query string. 
-  const urlParams = new URLSearchParams(window.location.search);
-  const imageParameter = urlParams.get('p');
-  console.log(imageParameter);
-
-
   if (imageParameter) {
     event.preventDefault();
     try {
