4c3ad49b81f84384f0dddf8825309a5b79855e4b
worldreset <112832918+worldreset@users.noreply.github.com>
2024-10-16 11:04:59 -0400

    Add files via upload

--- forestTownCode.js
+++ forestTownCode.js
@@ -14,50 +14,49 @@ function displayContent(content) {
   photo.setAttribute("alt", content.image.alt);
 }
 
-// When page is loaded, get p which is the query string. 
-const urlParams = new URLSearchParams(window.location.search);
-const imageParameter = urlParams.get('p');
-console.log(imageParameter);
-
-
+// When a link is clicked: update the page for that creature.
+// From a user perspective, this will look like the loading of a new page.
 document.addEventListener("click", async (event) => {
-  if (imageParameter) {
-    event.preventDefault();
-    try {
-      // When page is loaded, fetch the JSON associated with the query string, "p".
-      const response = await fetch(`forestImagesJSON/${imageParameter}.json`);
-
-      console.log(response);
-
-      const json = await response.json(); // continued from above
-      displayContent(json);
-      console.log(json);  
-    } catch (err) {
-      console.error(err);
-    }
+const dailypost = event.target.getAttribute("data-dailypost"); // a constant is created (creature) that contains attributes of data-creature in html.
+if (dailypost) {
+  event.preventDefault();
+  try {
+    const response = await fetch(`forestImagesJSON/${dailypost}.json`); // fetch the json from the specified creature. The file path is shown.
+    const json = await response.json(); // continued from above
+    displayContent(json);
+    // Add a new entry to the history.
+    // This simulates loading a new page.
+    history.pushState(json, "", dailypost);
+  } catch (err) {
+    console.error(err);
   }
+}
 });
 
-
-
-
-
-
-
-document.addEventListener("loadeddata", async (event) => {
-  if (imageParameter) {
-    event.preventDefault();
-    try {
-      // When page is loaded, fetch the JSON associated with the query string, "p".
-      const response = await fetch(`forestImagesJSON/${imageParameter}.json`);
-
-      console.log(response);
-
-      const json = await response.json(); // continued from above
-      displayContent(json);
-      console.log(json);  
-    } catch (err) {
-      console.error(err);
-    }
-  }
+// Handle forward/back buttons
+window.addEventListener("popstate", (event) => {
+// If a state has been provided, we have a "simulated" page
+// and we update the current page.
+if (event.state) {
+  // Simulate the loading of the previous page
+  displayContent(event.state);
+}
 });
+
+// Create state on page load and replace the current history with it
+// NOTE: Essentially, once the page starts up, it will make a URL and save it in history, which can be any animal page that is default.
+const image = document.querySelector("#comicThing");
+const initialState = {
+description: document.querySelector("#description").textContent,
+image: {
+  src: image.getAttribute("src"),
+  alt: image.getAttribute("alt"),
+},
+name: "Home",
+};
+history.replaceState(initialState, "", document.location.href);
+
+
+
+
+
